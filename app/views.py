1e42dc947937e01387b23a9f84f9a4edfed43b58
gluker <glukerr@gmail.com>
2015-07-28 15:44:12 +0000

    simple templates

--- app/views.py
+++ app/views.py
@@ -1,60 +1,70 @@
+from flask import render_template
 from . import app
 from . import models
+
+### dummy data for testing ###
+courses = [{'id':1, 'name':"Complicated Math"},{'id':2, 'name':"Even more complicated math"}]
+course = courses[0]
+psets = [{'id':1, 'title':"Firsr week of course", 'course_id':1},
+            {'id':2, 'title':"Second", 'course_id':1}]
+pset = psets[1]
+problems = [{'id':1,'text':"Text for 1<sup>st</sup> problem"},{'id':2,'text':"<h3>Problem</h3> blabla"}]
+problem = problems[0]
 
 @app.route('/')
 @app.route('/courses/')
 def showAllCourses():
-    return "List of all courses"
+    return render_template("showallcourses.html",courses=courses)
 
 ###  Views for courses ###
 @app.route('/courses/new/')
 def newCourse():
-    return "Page for creating a new course"
+    return render_template("newcourse.html")
 
 @app.route('/courses/<int:course_id>')
 @app.route('/courses/<int:course_id>/psets/')
 def showCourse(course_id):
-    return "Page for course %s" % course_id
+    return render_template("showcourse.html",course=course,psets=psets)
 
 @app.route('/courses/<int:course_id>/edit')
 def editCourse(course_id):
-    return "Page for editing course %s" % course_id
+    return render_template("editcourse.html",course=course)
 
 @app.route('/courses/<int:course_id>/delete/')
 def deleteCourse(course_id):
-    return "Page for deleting course %s" % course_id
+    return render_template("deletecourse.html",course=course)
 
 ### Views for Problem Sets ###
 @app.route('/courses/<int:course_id>/psets/new/')
 def newPSet(course_id):
-    return "create a new pset"
+    return render_template("newpset.html",course=course)
 
 @app.route('/courses/<int:course_id>/psets/<int:pset_id>/')
 @app.route('/course/<int:course_id>/psets/<int:pset_id>/problems/')
 def showPSet(course_id,pset_id):
-    return "Show pset %s" % pset_id 
+    return render_template("showpset.html",course=course,pset=pset,problems=problems)
 
 @app.route('/courses/<int:course_id>/psets/<int:pset_id>/edit/')
 def editPSet(course_id,pset_id):
-    return "edit pset"
+    return render_template("editpset.html",course=course,pset=pset)
 
 @app.route('/courses/<int:course_id>/psets/<int:pset_id>/delete/')
 def deletePSet(course_id,pset_id):
-    return "Delete PSet"
+    return render_template("deletepset.html",course=course,pset=pset)
 
 ### Views for Problems ###
 @app.route('/course/<int:course_id>/psets/<int:pset_id>/problems/new/')
-def newProblem(course_id, pset_id, problem_id):
-    return "Create a new Problem"
+def newProblem(course_id, pset_id):
+    return render_template("newproblem.html",course=course,pset=pset)
 
 @app.route('/courses/<int:course_id>/psets/<int:pset_id>/problems/<int:problem_id>/')
 def showProblem(course_id,pset_id,problem_id):
-    return "Show Problem"
+    return render_template("showproblem.html",course=course,pset=pset,problem=problem)
 
 @app.route('/courses/<int:course_id>/psets/<int:pset_id>/problems/<int:problem_id>/edit/')
 def editProblem(course_id,pset_id,problem_id):
-    return "Edit Problem"
+    return render_template("editproblem.html",course=course,pset=pset,problem=problem)
 
 @app.route('/courses/<int:course_id>/psets/<int:pset_id>/problems/<int:problem_id>/delete/')
 def deleteProblem(course_id,pset_id,problem_id):
-    return "Delete Problem"
+    return render_template("deleteproblem.html",course=course,pset=pset,problem=problem)
