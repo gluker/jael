040e620ac616a5a491cc2f54f606e82971ce1d34
gluker <glukerr@gmail.com>
2015-08-16 15:22:29 +0300

    remove requirement

--- app/static/js/newproblem.js
+++ app/static/js/newproblem.js
@@ -1,3 +1,6 @@
+removeRequirement = function(but){
+    $(but).parent().parent().remove();
+}
 $(function(){
    qCounter = +$("#counter").val()+1;
    var requirementBlock = ""+
@@ -7,6 +10,9 @@ $(function(){
             "</div>"+
             "<div class='col-xs-4'>"+
                 "<input placeholder='Comment for %id%'  class='form-control' type='text' name='requirements-%id%-comment'>"+
+            "</div>"+
+            "<div class='col-xs-1'>"+
+                "<button type='button' onclick='removeRequirement(this);'class='btn btn-danger'>X</button>"+
             "</div>"+
         "</div>";
     $("#addRequirementBtn").on('click',function(){
