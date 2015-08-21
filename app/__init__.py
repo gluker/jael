b9d889c27192c31f8e29beafc0028d57e99a4ddf
gluker <glukerr@gmail.com>
2015-08-21 20:51:00 +0300

    proxyfix

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
-
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__,instance_relative_config=True)
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
