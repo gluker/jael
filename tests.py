ac7db849fe60a15416aaefc5788a10fde7867306
gluker <glukerr@gmail.com>
2015-11-02 21:24:23 +0000

    more tests

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import os
-from app.utils import check_answer, check_input
+from app.utils import check_answer
 import app
 import unittest
 
@@ -9,7 +9,16 @@ class TestCheckAnswer(unittest.TestCase):
         self.assertTrue(check_answer("{v1} == 5",answer))
         self.assertTrue(check_answer("{v1} >= 3",answer))
         self.assertFalse(check_answer("{v1} > 5",answer))
-        
+    
+    def test_correctness(self):
+        answer = {"v2":"print(__globals__)"}
+        self.assertRaises(SyntaxError,check_answer,"{v2} != 7",answer)
 
+    def test_lim(self):
+        self.assertTrue(check_answer("limit({v1},x,0) == 0",{"v1":"sin(x)"}))
+        self.assertFalse(check_answer("limit({v1},x,0) == 0",{"v1":"cos(x)"}))
+        self.assertTrue(check_answer("limit({v1},x,oo) == 0",{"v1":"1/x"}))
+        self.assertTrue(check_answer("limit({v1},x,0) == oo",{"v1":"1/x"}))
+        
 if __name__ == '__main__':
     unittest.main()
