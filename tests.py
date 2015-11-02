a1ad1c6bf63969185bb116259fda443e13b11b05
gluker <glukerr@gmail.com>
2015-11-02 20:19:41 +0000

    tests for check

--- tests.py
+++ tests.py
@@ -1,25 +1,15 @@
 import os
+from app.utils import check_answer, check_input
 import app
 import unittest
-import urllib2
-import tempfile
-from flask_testing import LiveServerTestCase
-from flask import Flask
 
-class AppTestCase(LiveServerTestCase):
-    def create_app(self):
-        app = Flask(__name__)
-        app.config['DATABASE_URI'] = 'sqlite:///testdb.db'
-        app.config['TESTING'] = True
-        app.config['CSRF_ENABLED'] = False
-        app.config['LIVESERVER_PORT'] = 8943 
-        from app import views
-        return app
-    def test_server_is_up_and_running(self):
-        print self.get_server_url()
-        response = urllib2.urlopen(self.get_server_url())
-        self.assertEqual(response.code, 200)
-            
+class TestCheckAnswer(unittest.TestCase):
+    def test_numeric(self):
+        answer = {"v1":"5"}
+        self.assertTrue(check_answer("{v1} == 5",answer))
+        self.assertTrue(check_answer("{v1} >= 3",answer))
+        self.assertFalse(check_answer("{v1} > 5",answer))
+        
 
 if __name__ == '__main__':
     unittest.main()
