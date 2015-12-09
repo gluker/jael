c146b44599226d910f4cb28f4e98664eb53547c3
gluker <glukerr@gmail.com>
2015-12-09 10:14:57 +0000

    more tests

--- tests.py
+++ tests.py
@@ -1,7 +1,24 @@
 import os
 from app.utils import check_answer
-import app
+import app as jaot
 import unittest
+
+class BasicTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = jaot.app.test_client()
+    def test_index(self):
+        """inital test. ensure flask was set up correctly"""
+        response = self.app.get('/', content_type='html/text', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_nonexisting_course(self):
+        response = self.app.get('/course/999/')
+        self.assertEqual(response.status_code, 404)
+    
+    def test_course_url(self):
+        response = self.app.get('/course/1/', follow_redirects=True)
+        self.assertIn("Test course", response.data)
+
 
 class TestCheckAnswer(unittest.TestCase):
     def test_numeric(self):
@@ -21,6 +38,7 @@ class TestCheckAnswer(unittest.TestCase):
         self.assertFalse(check_answer("limit({v1},x,0) == 0",{"v1":"cos(x)"}))
         self.assertTrue(check_answer("limit({v1},x,oo) == 0",{"v1":"1/x"}))
         self.assertTrue(check_answer("limit({v1},x,0) == oo",{"v1":"1/x"}))
+        self.assertFalse(check_answer("limit({v1},x,oo) == oo",{"v1":"1/x"}))
         
 if __name__ == '__main__':
     unittest.main()
